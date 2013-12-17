158fb9152fbc92c78df18216816fba2e34e31286
khrnjak <krunoslav.hrnjak@gmail.com>
2013-12-17 12:59:45 +0100

    Sorted java properties list

---
+++ Props/src/Props.java
@@ -0,0 +1,19 @@
+import java.util.Iterator;
+import java.util.Properties;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class Props {
+	public static void main(String[] args) {
+		Properties systemProperties = System.getProperties();
+		SortedMap sortedSystemProperties = new TreeMap(systemProperties);
+		Set keySet = sortedSystemProperties.keySet();
+		Iterator iterator = keySet.iterator();
+		while (iterator.hasNext()) {
+			String propertyName = (String) iterator.next();
+			String propertyValue = systemProperties.getProperty(propertyName);
+			System.out.println(propertyName + ": " + propertyValue);
+		}
+	}
+}
